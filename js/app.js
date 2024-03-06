e5727656d7b1bbde5c089f6026a30fb4affede23
ferrarimurilo <159085219+ferrarimurilo@users.noreply.github.com>
2024-03-05 22:12:55 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+let membros = [];
+let membrosSorteados = document.getElementById("lista-sorteio");
+let numeroSorteios = 0;
+
+function adicionar(){
+    membros.push(document.getElementById('nome-amigo').value);
+    document.getElementById('nome-amigo').value = "";
+    console.log(membros);
+    document.getElementById("lista-amigos").textContent = membros
+}
+
+function reiniciar(){
+    membros = [];
+    document.getElementById("lista-amigos").textContent = "";
+    membrosSorteados.innerHTML = "";
+    numeroSorteios = 0;
+}
+
+function sortear(){
+    numeroSorteios++;
+    if(numeroSorteios>1){
+
+    }else{
+    embaralhar(membros);
+    console.log(membros);
+    for( i = 0; i < membros.length; i++){
+        if(i+1 == membros.length){
+            membrosSorteados.innerHTML = membrosSorteados.innerHTML + membros[i] + "-->" + membros[0]+'<br>';
+        }else{
+        membrosSorteados.innerHTML = membrosSorteados.innerHTML + membros[i] + "-->" + membros[i+1]+'<br>';
+    }}}
+}
+
+function embaralhar(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
